ffac7f10cc7ac204d1c1b958d2eb52437a40f3f2
tenaciousdeez <miles.haggerty@gmail.com>
2017-04-30 01:16:40 -0400

    Almost done restructuring the views to not contain any data. Currently data is in the controllers. Will be moved to the model later on. Need to consider the cookbook page at some point in the future.

--- app_server/controllers/dishes.js
+++ app_server/controllers/dishes.js
@@ -1,11 +1,76 @@
 //Get Home page
 module.exports.homeDishList = function(req, res, next){
-  res.render('dishes-list', { title: 'Home' });
+  res.render('dishes-list', {
+    title: "WICT - see what's cooking around you and show others what you've cooked!",
+    pageHeader: {
+      title: 'Dishes Near You',
+      strapline: 'See what home cooks around you are making!'
+    },
+    sidebar: "WICT lets you share images of what you cooked with those around you, as well as see what others are cooking.",
+    dishes: [{
+      name: 'Stir-fry Burrito Cake',
+      address: '-City, State-',
+      zip: '-zip code-',
+      categories: ['-category-', '-category-', '-category-'],
+      likes: '-like count-',
+      distance: '-distance to zip-',
+      coords: '',
+      images: [{}]
+    },{
+      name: 'Super Soggy Salad Sponge',
+      address: '-City, State-',
+      zip: '-zip code-',
+      categories: ['-category-', '-category-', '-category-'],
+      likes: '-like count-',
+      distance: '-distance to zip-',
+      coords: '',
+      images: [{}]
+    },{
+      name: 'Cajun Crab Cheese Dip',
+      address: '-City, State-',
+      zip: '-zip code-',
+      categories: ['-category-', '-category-', '-category-'],
+      likes: '-like count-',
+      distance: '-distance to zip-',
+      coords: '',
+      images: [{}]
+    }]
+  });
 };
 
 //Get Dish Info page
 module.exports.dishInfo = function(req, res, next){
-  res.render('dish-info', { title: 'Dish Info' });
+  res.render('dish-info', {
+    title: 'Szechuan McNuggets with the Szechuan McNugget Sauce',
+    pageHeader: {title: 'Szechuan McNuggets with the Szechuan McNugget Sauce'},
+    sidebar: {
+      context:'-user- cooked -dish title- on -date of submission-.',
+      callToAction: 'Want to share your thoughts on -author name-s -dish name-? Leave a comment!'
+    },
+    dish: {
+      name: 'Szechuan McNuggets with the Szechuan McNugget Sauce',
+      cook: 'Donkey Kong',
+      dateCreated: '6 June 2006',
+      address: '-City, State-',
+      zip: '-zip code-',
+      categories: ['-category1-', '-category2-', '-category3-'],
+      ingredients: ['-ingredient1-', '-ingredient2-', '-ingredient3-'],
+      instructions: ['-instruction1-', '-instruction2-', '-instruction3-'],
+      likes: '-like count-',
+      distance: '-distance to zip-',
+      coords: {lat: 38.924530, lng: -77.003058},
+      images: [{}],
+      comments: [{
+        author: 'Doug Dimmadome',
+        timestamp: '30 February 1844',
+        commentText: 'WOW! ALMOST AS GOOD AS MY DIMMSDALE DIMMADOME'
+      },{
+        author: 'Courage the Cowardly Dog',
+        timestamp: '12pm after the apocalypse',
+        commentText: 'AHHHHHHHHHHHHHHHH! OH GOD, OH GOD, OH GOD. AAGHGHGHGH!'
+      }]
+    }
+  });
 };
 
 //Get Add Comment page
